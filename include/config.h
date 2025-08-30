87073bce0e6b39161f40ef5788f1ba0ab3b3e514
ofranbraga <franciscocbbraga@gmail.com>
2025-08-30 15:28:48 +0000

    adicionado o codigo do config.h

---
+++ include/config.h
@@ -0,0 +1,13 @@
+#ifdef CONFIG_H
+#define CONFIG_H
+
+// Configuracoes da simulacao
+#define NUM_GARCONS 2
+#define NUM_COZINHEIROS_BIFE 2
+#define NUM_COZINHEIROS_SALADA 2
+#define NUM_CHEFS_MONTAGEM 1
+#define TOTAL_PEDIDOS 10
+#define TOTAL_PRATOS 6 //recurso limitado: numero de pratos limpos
+#define TAMANHO_QUADRO 10 //tamanho do buffer (fila) de pedidos
+
+#endif
